35b0b250eba8e5523c39ce551790eb0644e743e9
Raania1 <boujnehrania3@gmail.com>
2025-05-29 02:00:30 +0100

    update

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,7 +3,7 @@ import {ForgetPassword, login, resetPassword}  from "../controller/userControlle
 
 const router = Router()
 
-router.post("/auth/login",login)
+ router.post("/auth/login",login)
 router.post("/fogetpassword",ForgetPassword)
 router.post("/resetPassword/:id/:token",resetPassword)
 
