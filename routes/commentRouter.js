aba93ed8bf65bda9f18c902ff23b16935ee07e8d
Raania1 <boujnehrania3@gmail.com>
2025-06-11 00:36:39 +0100

    update

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import  { createComment,getCommentById ,getAllComments,updateComment,deleteComment} from "../controller/commentController.js";
+import { roleBasedAccess, verifyToken } from "../middleware/Authenticate.js";
 const router = Router();
 
-router.post('/create', createComment);
+router.post('/create',verifyToken,roleBasedAccess(["admin","organizer"]),createComment);
 router.get('/getById/:id', getCommentById);
 router.get('/getAll', getAllComments);
 router.put('/updateById/:id', updateComment);
